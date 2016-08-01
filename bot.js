8a6b583aa2a3defc9bdc73acc3b7b8ab948eb52a
SophiaRuns <sgfarquh@mtu.edu>
2016-08-01 15:15:22 -0500

    Update bot.js
    Changing the responses

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "I see you";
 
   options = {
     hostname: 'api.groupme.com',
